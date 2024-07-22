46eaf72b46b549d9c4be7b40b9cc5091e2da79c3
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-22 13:56:02 +0530

    Add files via upload

---
+++ stringsOfJs.js
@@ -0,0 +1,16 @@
+// let name = "Aditya"
+// let age = 25
+// let city = "Delhi"
+
+// console.log(`Hello, My name is ${name} and I am ${age} years old from ${city}`);
+
+const gameName = new String("  Aditya-Dheer-of-India   ")
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// let myIntroList = gameName.split("-")
+// console.log(myIntroList);
+
+console.log(gameName);
+console.log(gameName.trim());
