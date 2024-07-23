8840b4de51526b042c0d23e10b951fc1a2cadf20
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-23 13:44:32 +0530

    Add files via upload

---
+++ increment.js
@@ -0,0 +1,16 @@
+let i = 0;
+
+// console.log(++i === i++); // output is True.
+
+// console.log(i++ === ++i); // output is False.
+
+//prefix
+let a = ++i
+console.log("a", a);
+console.log("i", i);    // expected outcome is a = 1 and i = 1
+
+//postfix
+// let a = i++
+console.log("a", a);
+console.log("i", i);    // expected outcome is a = 0 and i = 1
+
---
+++ numbersOfJs.js
@@ -0,0 +1,21 @@
+let score = 400
+// console.log(score);
+
+let balance = new Number(100)
+// console.log(balance);
+
+let newNumber = 123.255154
+// console.log(typeof newNumber.toFixed(2)); //toFixed() will change the datatype from number to strings.
+
+let numTwo = 100000000
+// console.log(numTwo.toLocaleString());
+
+
+// Math.random will be the most used case in Js math.
+
+// go teg number in a range
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
