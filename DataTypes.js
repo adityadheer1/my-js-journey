2e53a53bc806963ea7ad6dea564e774bf5191ec2
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-23 13:46:44 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,8 @@
+let myName = "Aditya" //String
+let age = 11 //Number
+let isLoggedIn = true //Boolean
+
+console.table(typeof myName);
+console.table(typeof age);
+console.table(typeof isLoggedIn);
+
