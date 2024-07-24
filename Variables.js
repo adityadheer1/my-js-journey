b01404178a140f17d303875fdd4b12ab5e192424
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-24 11:04:04 +0530

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,11 @@
+const custmId = 123
+let custEmail = "customer@gmail.com"
+var custPassword = "abc123"
+custCity = "Delhi"
+
+// custmId = 321 #Changes were not allowed.
+custEmail = "customer@yahoo.com"
+custPassword = "123abc"
+custCity = "Mumbai"
+
+console.table([custmId, custEmail, custPassword, custCity]) // console.table will view all the params in a tabular structure.
