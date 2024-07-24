ea0abcb4d2835c39388a792a243d675334d36a98
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-24 11:04:51 +0530

    Add files via upload

---
+++ conversionOps.js
@@ -0,0 +1,18 @@
+let score = "33"
+// console.log(typeof score); // this will return type as string.
+
+let newScore = Number(score) // converted string 33 to Number 33
+// console.log(typeof newScore); 
+
+let loggedIn = "yes"
+
+let newLoggedIn = Boolean(loggedIn)
+console.log(newLoggedIn);
+
+/*
+In Boolean
+0 => False
+1 => True
+"" => False
+"Yes" => True
+*/
