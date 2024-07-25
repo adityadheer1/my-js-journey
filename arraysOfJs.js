ffa4aaa40b62e05ba689a48a622cb2314d65490f
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-25 12:06:39 +0530

    Add files via upload

---
+++ arraysOfJs.js
@@ -0,0 +1,44 @@
+let myFriends = ["Steve", "Jeff", "Elon", "Mark", "Bill"]
+
+// I want to call Elon from my array
+// console.log(myFriends[2]);
+
+// Ambani is my new friend, Lets add him
+myFriends.push("Ambani")
+
+// Me and Ambani are no longer friends, Lets remove him
+myFriends.pop() // will remove the value present at the end of the array.
+
+// Steve is no longer my best friend.
+myFriends.shift() // will remove the value present in the beginning of the array.
+
+// Tata is my best friend now.
+myFriends.unshift("Tata") // will add the value present at the end of the array.
+// console.log(myFriends);
+
+
+// Difference of Slice and Splice
+
+let myNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log("A" , myNum.slice(0, 5)); 
+// console.log(myNum);
+
+// console.log("B", myNum.splice(1, 5));
+// console.log(myNum);
+
+// Attach to or more arrays together.
+
+let marvelHeros = ["Ironman", "Thor", "Hulk"]
+let dcHeros =  ["Superman", "Batman", "Flash"]
+
+// push method
+// marvelHeros.push(dcHeros);
+// console.log(marvelHeros); // it will return nested array. An array within an array.
+
+// concate method
+// let assemble = marvelHeros.concat(dcHeros)
+// console.log(assemble); // will return a new array with both arrays added.
+
+// spread method
+let together = [ ...marvelHeros, ...dcHeros]
+console.log(together); // Will return same as concate but it is more simple to execute.
