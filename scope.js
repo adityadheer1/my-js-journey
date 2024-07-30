79bb110da444e4e31de21d42ea572b32d9b6ce45
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-30 13:28:14 +0530

    Add files via upload

---
+++ scope.js
@@ -0,0 +1,32 @@
+// if (true) {
+//     // let a = 10
+//     // const b = 20 // BLOCK SCOPE and anything outside this block is GLOBAL SCOPE
+//     // var c = 30
+// }
+
+//NESTED SCOPE
+
+// function one() {
+//     const username = "Aditya"
+
+//     function two() {
+//         const website = "youtube"
+//         // console.log(username);
+//     }
+//     // console.log(website);
+
+//     two()
+// }
+// one()
+
+// console.log(addone(5));
+
+// function addone(num) { // using this method won't return any error
+//     return num + 1
+// }
+
+console.log(addtwo(5));
+const addtwo = function(num){ // trying to define function this way will call an error if we call function before defining.
+    return num + 2
+}
+
