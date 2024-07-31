52e126218a49053f3e7cb987032eb5d9599c76a6
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-31 09:58:02 +0530

    Add files via upload

---
+++ controlFlow.js
@@ -0,0 +1,48 @@
+// IF
+// if(condition = true){
+//     scope
+// }
+
+// let month = 3
+
+// if (month == 1) {
+//     console.log("Jan");
+// } else if (month == 2) {
+//     console.log("Feb");
+// } else if (month == 3) {
+//     console.log("Mar");
+// } else if (month == 4) {
+//     console.log("Apr");
+// }
+
+//Sometimes its better to use Switch instead of IF condition
+
+/** SWITCH SYNTAX
+ * switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+ */
+let month = 4
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break; // break keyword helps in breaking the control flow. Without break all the subsequent commands will also run but not the DEFAULT
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default");
+        break;
+}
