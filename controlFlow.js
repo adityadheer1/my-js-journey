4037e8bf63e85e5fd60f8938bea66749e16ed88b
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-07-31 12:34:57 +0530

    controlFlow.js

--- controlFlow.js
+++ controlFlow.js
@@ -3,17 +3,19 @@
 //     scope
 // }
 
-// let month = 3
+let month = 3
 
-// if (month == 1) {
-//     console.log("Jan");
-// } else if (month == 2) {
-//     console.log("Feb");
-// } else if (month == 3) {
-//     console.log("Mar");
-// } else if (month == 4) {
-//     console.log("Apr");
-// }
+if (month == 1) {
+    console.log("Jan");
+} else if (month == 2) {
+    console.log("Feb");
+} else if (month == 3) {
+    console.log("Mar");
+} else if (month == 4) {
+    console.log("Apr");
+}
+
+// <, >, <=, >=, ==, !=
 
 //Sometimes its better to use Switch instead of IF condition
 
@@ -46,3 +48,14 @@ switch (month) {
         console.log("Default");
         break;
 }
+
+
+// TERNIATY OPERATOR
+
+// SYNTAX: condition ? true : false
+//example
+
+let loveMeter = 80
+
+loveMeter <= 50 ? console.log("your chances for a successful marriage is less than 50%") : console.log("your chances for a successful marriage is more than 50%"); 
+
