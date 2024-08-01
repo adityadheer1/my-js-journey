c30bb378df2b2d0b746ccc032dd731b380ec226c
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-08-01 19:51:03 +0530

    Add files via upload

---
+++ forOfLoop.js
@@ -0,0 +1,15 @@
+// FOR OF LOOP on Array
+
+// let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+
+// for (const days of weekDays) {
+//     console.log(`Today is ${days}`);
+// }
+
+// FOR OF LOOP on String
+
+let myName = "Aditya Dheer"
+
+for (const letter of myName) {
+    console.log(letter);
+}
