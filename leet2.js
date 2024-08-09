34afe4e6889851bb0b600490cb7f5e2e98688bf1
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-08-09 18:16:41 +0530

    Add files via upload

---
+++ leet2.js
@@ -0,0 +1,8 @@
+//2620 COUNTER.  Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+const createCounter = function(n) {
+    return function() {
+        return n++
+    };
+};
+
+createCounter(10)
