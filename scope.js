3759039c34c3c227f57b081b80ad28d940d07c6f
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-08-12 11:33:34 +0530

    scope.js

--- scope.js
+++ scope.js
@@ -1,34 +1,31 @@
-// Scopes work with Functions and conditionals
-
-// if (true) {
-//     // let a = 10
-//     // const b = 20 // BLOCK SCOPE and anything outside this block is GLOBAL SCOPE
-//     // var c = 30
-// }
+if (true) {
+    let a = 10
+    const b = 20 // BLOCK SCOPE and anything outside this block is GLOBAL SCOPE
+    var c = 30  // with using var to declare a variable 'c', it will defy the law of scopes and   will overflow from its block scope
+}
 
 //NESTED SCOPE
 
-// function one() {
-//     const username = "Aditya"
+function one() {
+    const username = "Aditya"
 
-//     function two() {
-//         const website = "youtube"
-//         // console.log(username);
-//     }
-//     // console.log(website);
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website); // this will return error as const website is in block scope.
 
-//     two()
-// }
-// one()
+    two()
+}
+one()
 
-// console.log(addone(5));
+console.log(addone(5));
 
-// function addone(num) { // using this method won't return any error
-//     return num + 1
-// }
-
-console.log(addtwo(5));
-const addtwo = function(num){ // trying to define function as an expression where a variable holds the function, this way will call an error if we call function before defining.
-    return num + 2
+function addone(num) { // using this method won't return any error
+    return num + 1
 }
 
+console.log(addtwo(5));
+const addtwo = function(num){ // trying to define function as an expression(where we use a variable to hold a function) where a variable holds the function, this way it will call an error if we call function before defining.
+    return num + 2
+}
