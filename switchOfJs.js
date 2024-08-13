ef66e86d84c2fb61448df6b70fea4a8721a8b80f
Aditya Dheer <99068686+adityadheer1@users.noreply.github.com>
2024-08-13 11:13:41 +0530

    Add files via upload

---
+++ switchOfJs.js
@@ -0,0 +1,20 @@
+let level = 5
+
+switch (level) {
+    case 1:
+        console.log("You are at level 1");
+        break;
+    case 2:
+        console.log("You are at level 2");
+        break;
+    case 3:
+        console.log("You are at level 3");
+        break;
+    case 4:
+        console.log("You are at level 4");
+        break;
+
+    default:
+        console.log("No level, But leval sabke niklenge");
+        break;
+}
